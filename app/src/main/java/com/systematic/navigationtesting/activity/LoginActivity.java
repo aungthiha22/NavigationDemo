f44d8c2f5988a5ce70616f50bc4bab51eeb7a62d
Shein <sheinthulwin@systematic-solution.com>
2020-01-20 16:07:34 +0630

    login create

--- app/src/main/java/com/systematic/navigationtesting/activity/LoginActivity.java
+++ app/src/main/java/com/systematic/navigationtesting/activity/LoginActivity.java
@@ -1,4 +1,19 @@
 package com.systematic.navigationtesting.activity;
 
-public class LoginActivity {
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.systematic.navigationtesting.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login_activity);
+
+
+    }
 }
