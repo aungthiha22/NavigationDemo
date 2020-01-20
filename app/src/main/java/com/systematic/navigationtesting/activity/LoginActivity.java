0362aba41982a6e8929b4519d0b1a272533865a7
Shein <sheinthulwin@systematic-solution.com>
2020-01-20 16:13:22 +0630

    login delete

--- app/src/main/java/com/systematic/navigationtesting/activity/LoginActivity.java
+++ app/src/main/java/com/systematic/navigationtesting/activity/LoginActivity.java
@@ -7,13 +7,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import com.systematic.navigationtesting.R;
 
-public class LoginActivity extends AppCompatActivity {
-
-    @Override
-    protected void onCreate(@Nullable Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.login_activity);
+public class LoginActivity  extends AppCompatActivity {
 
 
-    }
 }
